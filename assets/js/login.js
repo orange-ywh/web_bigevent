f21dbcd190f7c9d2d67b3addc0af6c8a01309041
ywh <1250260404@qq.com>
2020-12-30 13:58:26 +0800

    fix:删除了注册的根路径

--- assets/js/login.js
+++ assets/js/login.js
@@ -33,7 +33,7 @@ $(function () {
 
     $('#form_reg').on('submit', function (e) {
         var data = { username: $('#form_reg [name=username]').val(), password: $('#form_reg [name=password]').val() };
-        $.post('http://ajax.frontend.itheima.net/api/reguser', data, function (res) {
+        $.post('/api/reguser', data, function (res) {
             if (res.status !== 0) {
                 return layer.msg(res.message)
 
